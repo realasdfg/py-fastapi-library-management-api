34c2a5d7d6951bb31a9b2511df94cb1da05174ad
Roman Popadiuk <realasdfga@gmail.com>
2026-07-23 00:44:34 +0200

    Implement schemas for Author & Book models

---
+++ schemas.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class AuthorBaseSchema(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorSchema(AuthorBaseSchema):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class AuthorCreateSchema(AuthorBaseSchema):
+    pass
+
+
+class BookBaseSchema(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+
+
+class BookSchema(BookBaseSchema):
+    id: int
+    author: AuthorSchema
+
+    class Config:
+        from_attributes = True
+
+
+class BookCreateSchema(BookBaseSchema):
+    author_id: int
+    pass
